4b6ef1c8c39aa720b60a68f9c2a1c21e02c91516
dominicpam89 <dominicpam89@gmail.com>
2024-01-01 17:09:06 +0700

    added themeReducer

---
+++ src/data/store/theme.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit"
+import type { PayloadAction } from "@reduxjs/toolkit"
+
+type TypeThemeColors = "theme-light" | "theme-dark"
+type TypeThemeFonts = "font-theme-primary"|"font-theme-secondary"
+interface TypeThemeState{
+  colorTheme: TypeThemeColors,
+  fontTheme: TypeThemeFonts
+}
+
+const initialState: TypeThemeState = {
+  colorTheme: "theme-light",
+  fontTheme: "font-theme-primary"
+}
+
+const themeState = createSlice({
+  name: "theme",
+  initialState,
+  reducers:{
+    changeThemeColor: (state, action:PayloadAction<TypeThemeColors>)=>{
+      state.colorTheme = action.payload
+    },
+    changeThemeFonts: (state, action:PayloadAction<TypeThemeFonts>)=>{
+      state.fontTheme = action.payload
+    }
+  }
+})
+
+export const themeActions = themeState.actions
+export const themeReducer = themeState.reducer
