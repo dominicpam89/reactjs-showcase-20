c2726677bd59b12f73170250617417e9ba76907f
dominicpam89 <dominicpam89@gmail.com>
2024-01-01 20:41:09 +0700

    added ThemeProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx'
 import './index.css'
 import { store } from './data/store/index.ts'
 import { Provider as ReduxProvider } from "react-redux"
+import UIThemeProvider from './UI/ThemeProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ReduxProvider store={store}>
-      <App />
+      <UIThemeProvider>
+        <App />
+      </UIThemeProvider>
     </ReduxProvider>
   </React.StrictMode>,
 )
