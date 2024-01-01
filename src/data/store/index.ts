0183cb6d58927b3d6b6b4e88eb454978ea6017b7
dominicpam89 <dominicpam89@gmail.com>
2024-01-01 17:10:18 +0700

    added reducers, thunks and typeReferences in store

--- src/data/store/index.ts
+++ src/data/store/index.ts
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit"
+import { themeReducer } from "./theme"
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux"
 
 export const store = configureStore({
   reducer:{
-
+    theme: themeReducer
   },
 })
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+type AppDispatch = typeof store.dispatch
+export const useAppDispatch: ()=>AppDispatch = useDispatch
+
+type RootState = ReturnType<typeof store.getState>
+export const useAppSelector:TypedUseSelectorHook<RootState> = useSelector
