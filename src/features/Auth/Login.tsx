d2f02def6964df4d1a8609dc8efad0dd4d7e3373
dominicpam89 <dominicpam89@gmail.com>
2024-01-02 15:44:00 +0700

    added necessary files

---
+++ src/features/Auth/Login.tsx
@@ -0,0 +1,12 @@
+const AuthLogin = () => {
+  return <>
+    <form aria-label="form-login">
+      <div aria-label="input-group">
+        <label></label>
+        <input></input>
+      </div>
+    </form>
+  </>
+}
+ 
+export default AuthLogin;
---
+++ src/features/Auth/Register.tsx
@@ -0,0 +1,12 @@
+const AuthRegister = () => {
+  return <>
+    <form aria-label="form-register">
+      <div aria-label="input-group">
+        <label></label>
+        <input></input>
+      </div>
+    </form>
+  </>
+}
+ 
+export default AuthRegister;
