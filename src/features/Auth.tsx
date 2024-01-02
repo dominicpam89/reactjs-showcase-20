88a44c0301d30cf3619a7a25535171b1e209b733
dominicpam89 <dominicpam89@gmail.com>
2024-01-02 15:44:30 +0700

    modify features/Auth.tsx

--- src/features/Auth.tsx
+++ src/features/Auth.tsx
@@ -1,17 +1,30 @@
 import { useEffect } from "react"
 import { useSearchParams } from "react-router-dom"
-import UIContainer from "../UI/Container"
+import UIContainer from "../UI/Layout/Container"
+import UIStack from "../UI/Layout/Stack"
+import UIPageTitle from "../UI/PageTitle"
+import UIBrandLogo from "../UI/Brand/Logo"
+import UIBrandText from "../UI/Brand/Text"
 
 const Auth = () => {
-  const [searchParams, setSearchParams] = useSearchParams()
-  useEffect(()=>{
-    if(!searchParams.get("mode")) setSearchParams( "mode=login")
-  },[searchParams, setSearchParams])
-  return <>
-    <UIContainer aria-label="auth-container">
-      <p>Test</p>
-    </UIContainer>
-  </>
+	const [searchParams, setSearchParams] = useSearchParams()
+	useEffect(() => {
+		if (!searchParams.get("mode")) setSearchParams("mode=login")
+	}, [searchParams, setSearchParams])
+	return (
+		<>
+			<UIContainer aria-label="auth-container" color="primary">
+				<UIPageTitle
+					title="Trensy"
+					subtitle="an app where you can share your minds!"
+				/>
+				<UIStack customClass="justify-center items-center">
+					<UIBrandLogo />
+					<UIBrandText />
+				</UIStack>
+			</UIContainer>
+		</>
+	)
 }
- 
-export default Auth;
+
+export default Auth
