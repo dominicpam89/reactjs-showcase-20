ca46ffe2ad9bb4af09b4f5491fdb7e241a6c4580
dominicpam89 <dominicpam89@gmail.com>
2024-01-02 15:40:06 +0700

    added extension on Brand/Text.tsx

--- src/UI/Brand/Text.tsx
+++ src/UI/Brand/Text.tsx
@@ -1,14 +1,36 @@
 import { HTMLMotionProps, motion } from "framer-motion"
 
-interface Props extends HTMLMotionProps<"h1"|"h2"> {}
+interface Props extends HTMLMotionProps<"h1" | "h2"> {
+	weight?: "light" | "normal" | "bold" | "extrabold"
+	customClass?: string
+}
 
-const UIBrandText: React.FC<Props> = ({ ...defaultProps }) => {
+const twClasses = {
+	weight: {
+		light: "font-light",
+		normal: "font-normal",
+		bold: "font-bold",
+		extrabold: "font-extrabold",
+	},
+	size: "text-lg md:text-xl lg:text-3xl xl:text-6xl"
+}
+
+const UIBrandText: React.FC<Props> = ({
+	weight = "bold",
+	customClass = "",
+	...defaultProps
+}) => {
+	const _weight =
+		weight === "extrabold"
+			? twClasses.weight.extrabold
+			: weight === "bold"
+			? twClasses.weight.bold
+			: weight === "normal"
+			? twClasses.weight.normal
+			: twClasses.weight.light
 	return (
 		<>
-			<motion.h1
-				{...defaultProps}
-				className=""
-			>
+			<motion.h1 {...defaultProps} className={`${_weight} ${twClasses.size} ${customClass}`}>
 				{import.meta.env.VITE_APP_NAME}
 			</motion.h1>
 		</>
