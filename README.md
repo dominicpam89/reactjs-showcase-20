12f942d0ac875962c804e2ffe0b892895868c1db
dominicpam89 <dominicpam89@gmail.com>
2024-01-04 21:03:22 +0700

    added firebase package added queryClient and QueryClientProvider in main.tsx added README.md

---
+++ README.md
@@ -0,0 +1,25 @@
+Some features here:
+
+Register form
+	- SOLID principle
+	- react hook forms
+	- tailwind floating
+	- Firebase Authentication
+		- google account
+		- facebook account
+		- roles signup differences
+
+Signin form
+	- SOLID principle
+	- react hook forms
+	- tailwind floating
+	- Firebase Authentication
+		- google account
+		- facebook account
+		- roles signup differences
+
+Flexible Routes
+	- protected routes if not logged in
+	- logged in with welcome message @home
+	- profile settings
+	- redirect back to previous url after Authentication
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,19 @@ import App from './App.tsx'
 import './index.css'
 import { store } from './data/store/index.ts'
 import { Provider as ReduxProvider } from "react-redux"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import UIThemeProvider from './UI/ThemeProvider.tsx'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ReduxProvider store={store}>
-      <UIThemeProvider>
-        <App />
-      </UIThemeProvider>
-    </ReduxProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReduxProvider store={store}>
+        <UIThemeProvider>
+          <App />
+        </UIThemeProvider>
+      </ReduxProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
