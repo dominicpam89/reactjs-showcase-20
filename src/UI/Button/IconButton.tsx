541b8b413addf127dcd9b2185e86d6e929a2a6c7
dominicpam89 <dominicpam89@gmail.com>
2024-01-09 14:43:25 +0700

    modified .../Button/IconButton.tsx for text position

--- src/UI/Button/IconButton.tsx
+++ src/UI/Button/IconButton.tsx
@@ -4,12 +4,14 @@ import { motion, HTMLMotionProps } from "framer-motion"
 interface PropsButton extends HTMLMotionProps<"button"> {
 	icon: React.ReactNode
 	text?: string
+	textPos?: "left"|"right"
 	colorTwClass?: string
 	customClass?: string
 }
 const UIIconButton: React.FC<PropsButton> = ({
 	icon,
 	text="",
+	textPos="left",
 	colorTwClass = "inherit",
 	customClass = "",
 	...defaultProps
@@ -19,14 +21,15 @@ const UIIconButton: React.FC<PropsButton> = ({
 			{...defaultProps}
 			className={`flex space-x-1 items-center transition-none ${colorTwClass} ${customClass}`}
 			whileTap={{
-				scale: 0.95,
+				scale: 0.9,
 			}}
 			whileHover={{
 				scale: 1.1,
 			}}
 		>
+			{textPos==="left" && <span>{text}</span>}
 			<UIReactIcon icon={icon} className="w-full h-full" />
-			<span>{text}</span>
+			{textPos==="right" && <span>{text}</span>}
 		</motion.button>
 	)
 }
