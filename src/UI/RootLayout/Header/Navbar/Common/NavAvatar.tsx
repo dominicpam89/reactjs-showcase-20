1c8f76e85c8c24bf4b13b67395c193a012ba1da0
dominicpam89 <dominicpam89@gmail.com>
2024-01-09 16:31:18 +0700

    updated NavAvatar.tsx and NavCart.tsx

--- src/UI/RootLayout/Header/Navbar/Common/NavAvatar.tsx
+++ src/UI/RootLayout/Header/Navbar/Common/NavAvatar.tsx
@@ -1,10 +1,34 @@
+import { motion } from "framer-motion"
+import { useLocation, useNavigate } from "react-router-dom";
+
+const twClasses = {
+	common: "transition-all duration-300 ease-out",
+	avDef: "bg-primary-main-color/30",
+	avAct: "bg-primary-main-color"
+}
+
 const NavAvatar = () => {
-  return <>
-    <button className="relative rounded-full overflow-hidden h-10 w-10">
-      <img alt="avatar" src="/avatar-dummy.jpeg" className="w-full h-full" />
-      <div className="absolute top-0 left-0 w-full h-full bg-primary-main-color mix-blend-screen"></div>
-    </button>
-  </>
+  const navigate = useNavigate()
+	const {pathname} = useLocation()
+	const isActive = pathname==="/profile"
+	const commonClass = twClasses.common
+	const avatarClass = isActive ? twClasses.avAct : twClasses.avDef
+  return (
+		<>
+			<motion.button
+        onClick={()=>navigate("/profile")}
+				whileTap={{ scale: 0.95 }}
+				className={`relative rounded-full overflow-hidden h-12 w-12 ${commonClass}`}
+			>
+				<img
+					alt="avatar"
+					src="/avatar-dummy.jpeg"
+					className={`w-full h-full ${commonClass}`}
+				/>
+				<div className={`absolute top-0 left-0 w-full h-full mix-blend-screen ${avatarClass} ${commonClass}`}></div>
+			</motion.button>
+		</>
+  )
 }
  
 export default NavAvatar;
--- src/UI/RootLayout/Header/Navbar/Common/NavCart.tsx
+++ src/UI/RootLayout/Header/Navbar/Common/NavCart.tsx
@@ -1,82 +1,46 @@
 import UIReactIcon from "../../../../ReactIcon"
 import { FaCartShopping } from "react-icons/fa6"
-import { motion } from "framer-motion"
-import { useReducer } from "react"
-import { useLocation, useNavigate } from "react-router-dom"
-
-const twClasses = {
-  common:"transition-all duration-200 ease-out",
-  overlay:{
-    default: "-top-[50%] -left-[50%] h-[200%] w-[200%] border border-neutral-900/20 mix-blend-normal",
-    hovered: "-top-[60%] -left-[60%] h-[230%] w-[230%] border border-neutral-900/20 mix-blend-normal",
-    active: "-top-[60%] -left-[60%] h-[230%] w-[230%] bg-primary-main-color/40 border border-primary-main-color/30",
-  },
-  sticker:{
-    default: "w-[110%] h-[110%] bg-danger-main-color/80 text-danger-main-contrast",
-    hovered: "w-[125%] h-[125%] bg-danger-main-color/100 text-danger-main-contrast",
-    active: "w-[125%] h-[125%] bg-danger-main-color/80 text-danger-main-contrast",
-  },
-  icon:{
-    default: "h-full w-full",
-    hovered: "h-[110%] w-[110%]",
-    active: "h-full w-full"
-  }
-
-}
-
-type State = {
-	overlay: string
-	sticker: string
-  icon: string
-}
-
-type Action = {
-  type: "hovered"|"default"
-}
-
-const initialState:State = {
-  overlay: twClasses.overlay.default,
-  sticker: twClasses.sticker.default,
-  icon: twClasses.icon.default
-}
-
-const reducer = (state:State,action:Action)=>{
- state
- const {type} = action
- if(type==="hovered") return {
-  overlay: twClasses.overlay.hovered,
-  sticker: twClasses.sticker.hovered,
-  icon: twClasses.icon.hovered
- }
- else return {
-  overlay: twClasses.overlay.default,
-  sticker: twClasses.sticker.default,
-  icon: twClasses.icon.default
- }
-}
+import { AnimatePresence, motion } from "framer-motion"
+import useNavCart from "../../../../../data/hooks/useNavCart"
 
 const NavChart = () => {
-  const [state, dispatch] = useReducer(reducer,initialState)
-	const navigate = useNavigate()
-  const {pathname} = useLocation()
-  const overlay = pathname==="/cart" ? twClasses.overlay.active:state.overlay
-  const sticker = pathname==="/cart" ? twClasses.sticker.active:state.sticker
-  const icon = pathname==="/cart" ? twClasses.icon.active:state.icon
-  return (
-		<button className="relative h-5 w-5" 
-      onMouseOver={()=>dispatch({type:"hovered"})}
-      onMouseOut={()=>dispatch({type:"default"})}
-      onClick={()=>navigate("/cart")}
-    >
-			<motion.div
-				className={`absolute rounded-full ${twClasses.common} ${overlay}`}
-			></motion.div>
-			<div className={`absolute -top-[100%] left-[50%] rounded-full flex items-center justify-center ${twClasses.common} ${sticker}`}
+	const { navigate, commonClass, highlightClass, iconClass, isActive } =
+		useNavCart()
+	return (
+		<motion.button
+			whileTap={{ scale: 0.95 }}
+			className={`relative h-5 w-5 ${commonClass}`}
+			onClick={() => navigate("/cart")}
+		>
+			<AnimatePresence>
+				{isActive && (
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{
+							duration:0.66,
+						}}
+						className={`
+					absolute -top-[50%] -left-[50%] 
+					w-[200%] h-[200%] border rounded-full 
+					transition-none ${highlightClass}`}
+					></motion.div>
+				)}
+			</AnimatePresence>
+			<div
+				className={`
+					absolute -top-[80%] left-[50%] w-[110%] h-[110%] 
+					rounded-full flex items-center justify-center 
+					bg-danger-main-color text-danger-main-contrast ${commonClass}`}
 			>
 				3
 			</div>
-			<UIReactIcon icon={<FaCartShopping />} className={`${twClasses.common} ${icon}`} />
-		</button>
+			<UIReactIcon
+				icon={<FaCartShopping />}
+				className={`h-full w-full ${iconClass} ${commonClass}`}
+			/>
+		</motion.button>
 	)
 }
 
