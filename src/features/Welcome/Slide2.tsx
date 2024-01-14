f91b01695414585a20d82285162ed6f0a74e196f
dominicpam89 <dominicpam89@gmail.com>
2024-01-14 14:13:56 +0700

    establish abstract of ...Welcome/Slide2.tsx

--- src/features/Welcome/Slide2.tsx
+++ src/features/Welcome/Slide2.tsx
@@ -6,9 +6,12 @@ const WelcomeSlide2 = () => {
   return <>
     <UIStack
       centered="all"
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      exit={{opacity:0}}
     >
       <div aria-label="imageContainer" className="rounded-full w-[240px] h-[240px] overflow-hidden">
-        {imageState==="isLoading" && <p>skeleton image</p>}
+        {imageState==="isLoading" && <p>skeleton skeleton image</p>}
         {imageState==="isError" && <p>error message</p>}
         {imageState==="isReady" && <img src={imageQuery.data} alt="healthy food image small resolution" />}
       </div>
