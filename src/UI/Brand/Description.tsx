5c0458e31e31612ebd0a78f8173b26d148fc24ab
dominicpam89 <dominicpam89@gmail.com>
2024-01-17 23:05:55 +0700

    minor fix Brand/Description.tsx

--- src/UI/Brand/Description.tsx
+++ src/UI/Brand/Description.tsx
@@ -4,15 +4,11 @@ interface Props extends HTMLMotionProps<"p">{
 	customClass?: string
 }
 
-const twClasses = {
-	default: "transition-none"
-}
-
 const UIBrandDescription:React.FC<Props> = ({customClass="", ...defaultProps}) => {
 	return (
 		<motion.p
 			{...defaultProps}
-			className={`${twClasses.default} text-xs md:text-base lg:text-xl xl:text-2xl text-justify break-words hyphens-auto opacity-80 ${customClass}`}
+			className={`welcome_description ${customClass}`}
 		>
 			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
 			corporis ad quod doloremque neque accusantium molestiae repellat quam
