447839f36795372259fed76980609a0e1b429d9c
dominicpam89 <dominicpam89@gmail.com>
2024-01-23 22:30:28 +0700

    added signinWithGoogle functionality

--- src/data/hooks/useAuthSigninGoogle.ts
+++ src/data/hooks/useAuthSigninGoogle.ts
@@ -1,12 +1,20 @@
 import { authSigninGoogle } from "../services/auth"
-import { useQuery } from "@tanstack/react-query"
+import { useMutation } from "@tanstack/react-query"
+import { useNavigate } from "react-router-dom"
 
 const useAuthSigninGoogle = ()=>{
-  const {data, error, isError, isLoading} = useQuery({
-    queryKey: ["auth","google"],
-    queryFn: ()=>authSigninGoogle()
+  const navigate = useNavigate()
+  const {mutate:onSignIn, isPending, isError, isSuccess, isIdle, error:signInError} = useMutation({
+    mutationFn: ()=>authSigninGoogle(),
+    onSuccess: (data)=>{
+      localStorage.setItem("credential",JSON.stringify(data.credential))
+      localStorage.setItem("token",JSON.stringify(data.token))
+      localStorage.setItem("user",JSON.stringify(data.user[0]))
+      navigate("/")
+    }
   })
-  return {data, error, isError, isLoading}
+  const googleSignInState = {isPending, isError, isSuccess, isIdle}
+  return {onSignIn, googleSignInState, signInError}
 }
 
 export default useAuthSigninGoogle
